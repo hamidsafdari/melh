16483cd3380ca3a13feb3653c76ccaa185741e1c
Hamid Safdari <hamidsafdari22@gmail.com>
2022-08-13 06:46:45 +0430

    setup spring boot

---
+++ src/main/kotlin/hs/melh/Melh.kt
@@ -0,0 +1,11 @@
+package hs.melh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Melh
+
+fun main(args: Array<String>) {
+  runApplication<Melh>(*args)
+}
---
+++ src/test/kotlin/hs/melh/MelhTests.kt
@@ -0,0 +1,13 @@
+package hs.melh
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MelhTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
